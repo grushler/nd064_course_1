6f0b977da8cda75d031d12cbaec7becc94ce16f2
grushler <girishsortur@gmail.com>
2021-06-20 20:12:04 +0530

    Logging exercise solution branch

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+import logging
+logging.basicConfig(
+        format='[%(asctime)s] [%(levelname)s] [%(threadName)s] [%(name)s] [%(message)s]',
+        level=logging.DEBUG, 
+        filename='app.log')
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    app.logger.info("Homepage called successfully!")
+    return "Hello Udacity!"
+
+@app.route("/status")
+def healthCheck():
+    app.logger.info("Health check API called successfully!")
+    data = {"result": "OK: Healthy"}
+    return jsonify(data), 200
+
+@app.route("/metrics")
+def metrics():
+    app.logger.info("Metrics API called successfully!")
+    data = {"status": "success", "data": {"UserCount": 100, "UserCountActive": 20}}
+    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
